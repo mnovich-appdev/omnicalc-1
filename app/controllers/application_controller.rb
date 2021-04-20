43f4afa342c9c29a64b4fa50da96249c2df86e1a
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-20 16:22:07 +0000

    random draft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,8 @@ class ApplicationController < ActionController::Base
   end
 
   def calculate_square
-
     @num = params.fetch("squarenumber").to_f
     @square_of_num = @num * @num
-
     render({:template => "calculation_templates/square_results.html.erb"})
   end
 
@@ -17,11 +15,28 @@ class ApplicationController < ActionController::Base
   end
 
   def calculate_square_root
-   
     @root_num = params.fetch("rootnumber").to_f
     @root_of_root_num = @root_num ** 0.5
+    render({:template => "calculation_templates/square_root_results.html.erb"})
+  end
 
-     render({:template => "calculation_templates/square_root_results.html.erb"})
+  def blank_payment_form
+    render({:template => "calculation_templates/payment_form.html.erb"})
+  end
+
+  def calculate_payment
+    render({:template => "calculation_templates/payment_results.html.erb"})
+  end
+
+  def blank_random_form
+    render({:template => "calculation_templates/random_form.html.erb"})
+  end
+
+  def calculate_random
+    render({:template => "calculation_templates/random_results.html.erb"})
+    @min_num = params.fetch("min_num").to_f
+    @max_num = params.fetch("max_num").to_f
+    @rand_number = rand(@min_num..@max_num)
   end
 
 end
